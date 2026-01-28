e24c63688908be35083243ca13b8e32e7c53a781
Paula-Refaat <poularefaat86@gmail.com>
2026-01-28 16:04:40 +0200

    🐛 Add MongoDB service to docker-compose and integrate Mongoose for database connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const mongoose = require("");
 
+// init app
+const PORT = Process.env.PORT || 4000;
 const app = express();
 
+// connect db
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_Host = 'mongo';
+
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_Host}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("connect to db ....");
+  })
+  .catch((err) => console.log("failed to connect to db  ", err));
 app.get("/", (req, res) => {
   res.send("Hello, Worlds!");
 });
 
-const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
