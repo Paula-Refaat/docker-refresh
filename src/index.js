5dedf08db4dd478095141a0a9b35140175ff1e9c
Paula-Refaat <poularefaat86@gmail.com>
2026-01-28 20:02:51 +0200

    🐛 Add Redis service to docker-compose and integrate Redis client in the application

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const mongoose = require("");
+const redis = require("redis");
 
 // init app
 const PORT = Process.env.PORT || 4000;
 const app = express();
 
+// connect redis
+const redisClient = redis.createClient();
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () => console.log("Redis Client Connected"));
+redisClient.connect();
+
 // connect db
 const DB_USER = "root";
 const DB_PASSWORD = "example";
 const DB_PORT = 27017;
-const DB_Host = 'mongo';
-
+const DB_Host = "mongo";
 
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_Host}:${DB_PORT}`;
 mongoose
